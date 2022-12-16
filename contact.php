34c8a47e0e3f123f6f9f6dee2b9a260625594116
Keerthana <keerthanaturlapati@gmail.com>
2022-12-15 19:50:48 -0500

    adding contact us page

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/cart.css">
+
+    <title>KT's JEWELRY STORE</title>
+</head>
+<body>
+    <?php require "./snippets/_navbar.php"; ?> 
+    <div class="container">
+        <div class="cart-container">
+            <p>About US : We are an online Jewelry store and only accpet online orders</p><br>
+            <p>We sell Rings, Necklaces, Earrings and Bracelets</p><br>
+            <p> Contact US: </p>
+            <p> Phone Number : 99999999 </p>
+            <p> Email Address : abc@gmail.com </p>
+        </div>
+
+    </div>
+    <footer>
+
+    </footer>
+<script src="https://kit.fontawesome.com/cc211e9abe.js" crossorigin="anonymous"></script>   
+</body>
+</html>
--- snippets/_navbar.php
+++ snippets/_navbar.php
@@ -14,10 +14,11 @@
             
         <div class="auth">
         <a href="./cart.php" class="nav__links">Cart</a>
+        <a href="./contact.php" class="nav__links">Contact US</a>
         <?php
             if($display_btn){
                 echo('<a href="./register.php" class="nav__links">Register</a>
-                <a href="./login.php" class="nav__links">Login</a>');
+                <a href="./login.php" class="nav__links">Login</a> <a href="./contact.php" class="nav__links">Contact US</a>');
             }
             else{
             echo('<a href="./logout.php" class="nav__links logout-btn">Logout</a>');
